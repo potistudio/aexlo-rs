23cd36f66f44f11f103e93dc0d911b5e95add1ec
potistudio <potistudio@proton.me>
2025-06-09 23:18:52 +0900

    ✨ Update src/main.rc

--- src/main.rs
+++ src/main.rs
@@ -8,49 +8,50 @@ use dlopen::wrapper::{ Container, WrapperApi };
 const BASE_PATH: &str = "./test";
 const MODULE_NAME: &str = "SDK_Noise";
 
-extern "C" fn test_sub_fn (a: i32, b: i32) -> i32 { a - b }
+//* Examples */
+// extern "C" fn test_sub_fn (a: i32, b: i32) -> i32 { a - b }
 
-#[repr(C)]
-pub struct TestInData {
-	pub sub: Option<unsafe extern "C" fn (a: i32, b: i32) -> i32>,
-}
+// #[repr(C)]
+// pub struct TestInData {
+// 	pub sub: Option<unsafe extern "C" fn (a: i32, b: i32) -> i32>,
+// }
 
-#[derive(WrapperApi)]
-pub struct TestDllApi {
-	Add: fn (a: i32, b: i32) -> i32,
-	CallSub: fn (in_data: &TestInData) -> i32,
-}
+// #[derive(WrapperApi)]
+// pub struct TestDllApi {
+// 	Add: fn (a: i32, b: i32) -> i32,
+// 	CallSub: fn (in_data: &TestInData) -> i32,
+// }
 
-fn call_test_dll() {
-	let mut module_path: String = String::from("");  // default format is windows dll
-	let os = std::env::consts::OS;
+// fn call_test_dll() {
+// 	let mut module_path: String = String::from("");  // default format is windows dll
+// 	let os = std::env::consts::OS;
 
-	match os {
-		"windows" => { module_path = format!("{}/{}.dll", BASE_PATH, MODULE_NAME); },
-		"macos" => { module_path = format!("{}/{}.plugin/Contents/MacOS/{}", BASE_PATH, MODULE_NAME, MODULE_NAME); },
-		_ => { panic!("Cannot detect OS. Supported OS are 'windows' and 'macos'."); },
-	};
+// 	match os {
+// 		"windows" => { module_path = format!("{}/{}.dll", BASE_PATH, MODULE_NAME); },
+// 		"macos" => { module_path = format!("{}/{}.plugin/Contents/MacOS/{}", BASE_PATH, MODULE_NAME, MODULE_NAME); },
+// 		_ => { panic!("Cannot detect OS. Supported OS are 'windows' and 'macos'."); },
+// 	};
 
-	println!("<aexlo> [INFO]  - Detected OS: {}", os);
-	println! ("<aexlo> [INFO]  - Loading library: {} from {}", MODULE_NAME, module_path);
+// 	println!("<aexlo> [INFO]  - Detected OS: {}", os);
+// 	println! ("<aexlo> [INFO]  - Loading library: {} from {}", MODULE_NAME, module_path);
 
-	//* Load DLL
-	let container: Container<TestDllApi> = unsafe {
-		Container::load (module_path)
-	}.expect ("Cannot load library");
-
-
-	//* Call Function
-	let test_add_result: i32 = container.Add (1, 2);  // expect 3
-	println! ("Add result: {}", test_add_result);
+// 	//* Load DLL
+// 	let container: Container<TestDllApi> = unsafe {
+// 		Container::load (module_path)
+// 	}.expect ("Cannot load library");
 
 
-	//* Apply Callback and Call
-	let in_data: TestInData = TestInData { sub: Some(test_sub_fn) };
-	let test_sub_result: i32 = container.CallSub (&in_data);
+// 	//* Call Function
+// 	let test_add_result: i32 = container.Add (1, 2);  // expect 3
+// 	println! ("Add result: {}", test_add_result);
 
-	println! ("Call Sub result: {}", test_sub_result);
-}
+
+// 	//* Apply Callback and Call
+// 	let in_data: TestInData = TestInData { sub: Some(test_sub_fn) };
+// 	let test_sub_result: i32 = container.CallSub (&in_data);
+
+// 	println! ("Call Sub result: {}", test_sub_result);
+// }
 
 
 #[derive(WrapperApi)]
