78911526f14ecafde4f88984ea54ab7c6c8e41c3
potistudio <potistudio@proton.me>
2025-06-10 12:09:52 +0900

    🔥 remove example code

--- src/main.rs
+++ src/main.rs
@@ -11,51 +11,6 @@ use std::ffi::{CStr, CString};
 const BASE_PATH: &str = "./test";
 const MODULE_NAME: &str = "SDK_Noise";
 
-//* Examples */
-// extern "C" fn test_sub_fn (a: i32, b: i32) -> i32 { a - b }
-
-// #[repr(C)]
-// pub struct TestInData {
-// 	pub sub: Option<unsafe extern "C" fn (a: i32, b: i32) -> i32>,
-// }
-
-// #[derive(WrapperApi)]
-// pub struct TestDllApi {
-// 	Add: fn (a: i32, b: i32) -> i32,
-// 	CallSub: fn (in_data: &TestInData) -> i32,
-// }
-
-// fn call_test_dll() {
-// 	let mut module_path: String = String::from("");  // default format is windows dll
-// 	let os = std::env::consts::OS;
-
-// 	match os {
-// 		"windows" => { module_path = format!("{}/{}.dll", BASE_PATH, MODULE_NAME); },
-// 		"macos" => { module_path = format!("{}/{}.plugin/Contents/MacOS/{}", BASE_PATH, MODULE_NAME, MODULE_NAME); },
-// 		_ => { panic!("Cannot detect OS. Supported OS are 'windows' and 'macos'."); },
-// 	};
-
-// 	println!("<aexlo> [INFO]  - Detected OS: {}", os);
-// 	println! ("<aexlo> [INFO]  - Loading library: {} from {}", MODULE_NAME, module_path);
-
-// 	//* Load DLL
-// 	let container: Container<TestDllApi> = unsafe {
-// 		Container::load (module_path)
-// 	}.expect ("Cannot load library");
-
-
-// 	//* Call Function
-// 	let test_add_result: i32 = container.Add (1, 2);  // expect 3
-// 	println! ("Add result: {}", test_add_result);
-
-
-// 	//* Apply Callback and Call
-// 	let in_data: TestInData = TestInData { sub: Some(test_sub_fn) };
-// 	let test_sub_result: i32 = container.CallSub (&in_data);
-
-// 	println! ("Call Sub result: {}", test_sub_result);
-// }
-
 unsafe extern "C" fn sprintf(arg1: *mut after_effects_sys::A_char, arg2: *const after_effects_sys::A_char, mut args: ...) -> i32 {
 	unsafe {
 		let format_str = CStr::from_ptr(arg2 as *const i8).to_string_lossy();
@@ -324,10 +279,6 @@ fn call_plugin() -> Result<(), dlopen::Error> {
 		dephault: 0,
 	};
 	println! ("[INFO] - Plugin parameters initialized");
-
-	// Inject Interact Callbacks
-	// let in_data: TestInData = TestInData { sub: Some(test_sub_fn) };
-	// let test_sub_result: i32 = container.CallSub (&in_data);
 
 	// Load DLL
 	let container: Container<EffectMain> = unsafe {
